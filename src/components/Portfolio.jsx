abb87461b27c97f6cb109696f9beb9c3b98a9a73
pandeyprashant123-coder <83771967+pandeyprashant123-coder@users.noreply.github.com>
2023-02-21 12:18:17 +0545

    projects

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,27 +1,76 @@
-import React from 'react'
-import Kosthetic from '../imgs/bgi.jpg'
+import React from "react";
+import Kosthetic from "../imgs/bgi.jpg";
 
+const Card = ({img,link,clink,name,description}) => {
+  return(
+  <>
+    <img
+      src={img}
+      alt=""
+      className="rounded-2xl drop-shadow-md"
+    />
+    <div className="cardcontent flex -translate-y-32 text-white rounded-2xl">
+      <div className="pb-3 pl-1">
+        <h3 className="text-xl font-bold">{name}</h3>
+        <p className="">{description}</p>
+        <a href={clink}><button>Viewcode</button></a>
+        <a href={link} rel="noreferrer" target={"_blank"}><button>visit site</button></a>
+      </div>
+    </div>
+  </>
+)};
+
+// "https://social-site-ddb414.netlify.app/"
 const Portfolio = () => {
   return (
-    <section className='bg-[#89618A] z-0'>
-        <div className='bottom-gradient flex flex-col  px-28 pb-8 pt-6'>
-            <h1 className='text-8xl font-medium text-white mb-11'>My Portfolio</h1>
-            <h1 className='text-5xl font-medium text-white text-center mb-11'>My masterpiece collection</h1>
-            <div className='grid grid-cols-4 gap-6 my-6'>
-                <a href='https://pandeyprashant123-coder.github.io/kosthetik/' className='' target={'_blank'}> <img src={Kosthetic} className='rounded-2xl drop-shadow-md'  alt="" height='100%' width='100%'/></a>
-                <a href='https://hackathon-indol.vercel.app/' className='' target={'_blank'}> <img src='https://cdn.britannica.com/89/140889-050-EC3F00BF/Ripening-heads-rice-Oryza-sativa.jpg' className='rounded-2xl drop-shadow-md' alt="" height='100%' width='100%'/></a>
-                <a href='https://github.com/fairfield-programming'  target={'_blank'}> <img src='https://github.com/fairfield-programming/.github/raw/main/spread.png?raw=true' className='rounded-2xl drop-shadow-md' alt="" height='100%' width='100%'/></a>
-                <a href='https://pandeyprashant123-coder.github.io/ACES-PAGE.github.io/'  target={'_blank'}> <img src='https://github.com/pandeyprashant123-coder/ACES-PAGE.github.io/blob/main/Images/web%20design%20compition.jpg?raw=true' className='rounded-2xl drop-shadow-md' alt="" height='100%' width='100%'/></a>
-                <a href='https://social-site-ddb414.netlify.app/' target={'_blank'}> <img src={Kosthetic} className='rounded-2xl drop-shadow-md' alt="" height='100%' width='100%'/></a>
-                <a href='' target={'_blank'}> <img src="" className='rounded-2xl drop-shadow-md' alt="" height='100%' width='100%'/></a>
-                <a href='' target={'_blank'}> <img src={Kosthetic} className='rounded-2xl drop-shadow-md' alt="" height='100%' width='100%'/></a>
-                <a href='' target={'_blank'}> <img src={Kosthetic} className='rounded-2xl drop-shadow-md' alt="" height='100%' width='100%'/></a>
-                <a href='' target={'_blank'}> <img src={Kosthetic} className='rounded-2xl drop-shadow-md' alt="" height='100%' width='100%'/></a>
-                <a href='' target={'_blank'}> <img src={Kosthetic} className='rounded-2xl drop-shadow-md' alt="" height='100%' width='100%'/></a>
-            </div>
+    <section className="bg-[#89618A] z-0">
+      <div className="bottom-gradient flex flex-col  px-28 pb-8 pt-6">
+        <h1 className="text-8xl font-medium text-white mb-11">My Portfolio</h1>
+        <h1 className="text-5xl font-medium text-white text-center mb-11">
+          My masterpiece collection
+        </h1>
+        <div className="grid grid-cols-7 gap-3 my-6">
+          <div className="col-span-3 row-span-2">
+            <Card
+              img={'https://images.pexels.com/photos/3184435/pexels-photo-3184435.jpeg?auto=compress&cs=tinysrgb&w=600'}
+              link={'https://social-site-ddb414.netlify.app/'}
+              name={'social-Site'}
+              clink={'https://social-site-ddb414.netlify.app/'}
+              description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, quae."}
+            />
+          </div>
+          <div className="col-span-2">
+            <Card
+              img={Kosthetic}
+              link={'https://pandeyprashant123-coder.github.io/kosthetik/'}
+              name={'Kosthetik'}
+              clink={'https://pandeyprashant123-coder.github.io/kosthetik/'}
+              description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, quae."}
+            />
+          </div>
+          <div className="col-span-2">
+            <Card
+              img={'https://cdn.britannica.com/89/140889-050-EC3F00BF/Ripening-heads-rice-Oryza-sativa.jpg'}
+              link={'https://hackathon-indol.vercel.app/'}
+              name={'Hackathon'}
+              clink={'https://hackathon-indol.vercel.app/'}
+              description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, quae."}
+            />
+          </div>
+          <div className="col-span-4">
+            <Card
+              img={'https://github.com/fairfield-programming/.github/raw/main/spread.png?raw=true'}
+              link={'https://github.com/fairfield-programming'}
+              name={'Opensource'}
+              clink={'https://github.com/fairfield-programming'}
+              description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, quae."}
+            />
+          </div>
+          
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
