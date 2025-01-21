ebb9a201189a4cfb848e7f95c871fad690a75f71
pandeyprashant123-coder <83771967+pandeyprashant123-coder@users.noreply.github.com>
2025-01-21 12:12:46 +0545

    style changes

--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -13,7 +13,7 @@ const Card = ({ img, link, clink, name, description }) => {
         <Image
           src={img}
           alt=""
-          className="drop-shadow-md h-40 lg:h-96 object-cover lg:w-1/2 m-2 rounded-md"
+          className="drop-shadow-md h-40 lg:h-80 object-cover lg:w-1/2 m-2 rounded-md"
           width={500}
           height={0}
         />
@@ -22,22 +22,26 @@ const Card = ({ img, link, clink, name, description }) => {
           {name}
         </h1>
       )}
-      <div className=" flex text-white lg:w-1/2 p-5 items-center justify-start">
+      <div className=" flex text-white lg:w-1/2 lg:p-5 items-center justify-start">
         <div className=" px-3">
           {img && <h3 className="text-xl font-bold">{name}</h3>}
           <p className="lg:pb-5">{description}</p>
-          {link && (
-            <Link href={link}>
-              <button className="bg-white text-black rounded p-1 mb-1 mr-2">
-                See demo
-              </button>
-            </Link>
-          )}
-          {clink && (
-            <Link href={clink} rel="noreferrer" target={"_blank"}>
-              <button>View code</button>
-            </Link>
-          )}
+          <div className="flex gap-2 my-3 items-center">
+            {link && (
+              <Link href={link}>
+                <button className="bg-white text-black font-semibold rounded py-1 px-3  mr-2 duration-150 hover:bg-gray-100">
+                  See demo
+                </button>
+              </Link>
+            )}
+            {clink && (
+              <Link href={clink} rel="noreferrer" target={"_blank"}>
+                <button className="hover:bg-gray-200 py-1 px-3 rounded hover:text-black duration-150">
+                  View code
+                </button>
+              </Link>
+            )}
+          </div>
         </div>
       </div>
     </div>
